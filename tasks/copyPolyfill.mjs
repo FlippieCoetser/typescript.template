d22c406d04628dce43721d5aef5c700d4be419ba
Flippie.Coetser <flippie.coetser@gmail.com>
2022-06-06 04:02:18 -0700

    add copy polyfill to directory

---
+++ tasks/copyPolyfill.mjs
@@ -0,0 +1,24 @@
+import process from 'node:process'
+import { walk } from './walk.mjs'
+
+import { copyFile } from 'node:fs/promises'
+
+let basePath = ''
+let directory = `${process.env.INIT_CWD}${basePath}/`
+
+let directoriesWithImportmap = (directory) =>
+    walk(directory, '*.importmap')
+    .map(file => file.replace(/[^/]*$/,''))
+
+let directories = directoriesWithImportmap(directory)
+
+let copyExtImportmapPolyfill = async (directories) => {
+    directories.forEach( async directory => {
+        await copyFile(`./importmap/importmap.js`, `${directory}importmap.js`);
+    })
+}
+
+copyExtImportmapPolyfill(directories)
+
+//Read importmap 
+//check if module defined, if not create
