dee0c7778f3e034e57ab811f73fd3c1080f86cf1
Flippie.Coetser <flippie.coetser@gmail.com>
2022-06-06 04:02:42 -0700

    Remove legacy

--- tasks/script.mjs
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env node
-import * as readline from 'node:readline/promises';
-import { stdin as input, stdout as output } from 'node:process';
-
-const rl = readline.createInterface({ input, output });
-const answer = await rl.question('Enter path to html document:');
-console.log(`importmap lib copied into: ${answer}`);
-rl.close();
