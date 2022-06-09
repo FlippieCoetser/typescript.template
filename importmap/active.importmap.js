59da836b006338e9924d23934bc863fa64b71844
Flippie.Coetser <flippie.coetser@gmail.com>
2022-06-09 07:36:00 -0700

    Move Importmap Scripts

---
+++ importmap/active.importmap.js
@@ -0,0 +1,4 @@
+const getImportMap = () => document.querySelectorAll('[type="importmap"]');
+const map = getImportMap()
+
+console.log(map[0])
--- importmap/importmap.js
+++
@@ -1,8 +0,0 @@
-const script = document.createElement('script');
-script.type = 'importmap';
-script.textContent = JSON.stringify({
-    "imports": {
-        "calculator": "../lib/Calculator.js"
-    }
-});
-document.currentScript.after(script);
